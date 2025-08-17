f420b85f08d31b85e4273aaab6163a196c33437e
Ben Schembri <benschembri@gmail.com>
2025-08-17 23:12:49 +1000

    allow multiple connections in parallel

--- server.rb
+++ server.rb
@@ -6,35 +6,39 @@ PORT = 54321
 server = TCPServer.new(HOST, PORT)
 puts "Server running on #{HOST}:#{PORT}"
 
-socket = server.accept
-puts 'Client connected!'
-
-# Simulate a TCP Handshake
-syn = socket.gets&.chomp
-puts "Received #{syn}"
-
-puts 'Sending SYN-ACK...'
-syn_ack = "[SYN-ACK] Acknowledge recieving SYN: #{syn}"
-socket.puts syn_ack
-
-ack = socket.gets&.chomp
-puts "Recieved ACK #{ack}"
-
-puts ' TCP socket connection established.'
-puts ' Waiting for query...'
-###
-
 loop do
-  socket.puts 'Enter a query (or type DISCONNECT):'
-  query = socket.gets.chomp
-  puts "Received #{query}"
-  break if query.nil? || query.chomp == 'DISCONNECT'
+  client_socket = server.accept
+  Thread.new(client_socket) do |socket|
+    puts 'Client connected!'
 
-  puts "Performing #{query}..."
-  result = query
-  puts "Sending result: #{result}"
-  socket.puts "[RESULT] #{result}"
+    # Simulate a TCP Handshake
+    syn = socket.gets&.chomp
+    puts "Received #{syn}"
+
+    puts 'Sending SYN-ACK...'
+    syn_ack = "[SYN-ACK] Acknowledge recieving SYN: #{syn}"
+    socket.puts syn_ack
+
+    ack = socket.gets&.chomp
+    puts "Recieved ACK #{ack}"
+
+    puts ' TCP socket connection established.'
+    puts ' Waiting for query...'
+    ###
+
+    loop do
+      socket.puts 'Enter a query (or type DISCONNECT):'
+      query = socket.gets.chomp
+      puts "Received #{query}"
+      break if query.nil? || query.chomp == 'DISCONNECT'
+
+      puts "Performing #{query}..."
+      result = query
+      puts "Sending result: #{result}"
+      socket.puts "[RESULT] #{result}"
+    end
+
+    puts "Client disconnected."
+    socket.close
+  end
 end
-
-puts "Client disconnected."
-socket.close
